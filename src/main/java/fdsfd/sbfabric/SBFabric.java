4b88c8f67e5165fedba686fc619236e30dde5cde
ssingh0826 <sehajsingh4293+GitHub@gmail.com>
2025-05-22 20:01:22 -0400

    we're getting somewhere

--- src/main/java/fdsfd/sbfabric/SBFabric.java
+++ src/main/java/fdsfd/sbfabric/SBFabric.java
@@ -1,24 +1,42 @@
 package fdsfd.sbfabric;
 
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import fdsfd.sbfabric.utils.PingUtils;
 import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import static net.minecraft.server.command.CommandManager.*;
 
 public class SBFabric implements ModInitializer {
 	public static final String MOD_ID = "sbfabric";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("SBFabric is initializing!");
+
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			final LiteralCommandNode<ServerCommandSource> sbfNode = dispatcher.register(literal("sbfabric")
+					.executes(context -> {
+						context.getSource().sendFeedback(() -> Text.literal("Mod Menu coming soontm"), true);
+
+						return 1;
+						})
+					.then(literal("ping")
+							.executes(context -> {
+								PingUtils.displayPing();
+								return 1;
+							})
+						)
+			);
+			dispatcher.register(literal("sbf").redirect(sbfNode));
+		});
+
+		LOGGER.info("SBFabric is done initializing!");
 	}
 }
---
+++ src/main/java/fdsfd/sbfabric/utils/PingUtils.java
@@ -0,0 +1,41 @@
+package fdsfd.sbfabric.utils;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
+public class PingUtils {
+
+    public static int displayPing() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        ClientPlayNetworkHandler networkHandler = client.getNetworkHandler();
+        if (networkHandler != null) {
+            PlayerListEntry playerListEntry = networkHandler.getPlayerListEntry(client.player.getUuid());
+            if (playerListEntry != null) {
+                int ping = playerListEntry.getLatency();
+                client.player.sendMessage(
+                    Text.literal("SBFabric").formatted(Formatting.BOLD).formatted(Formatting.BLUE)
+                        .append(Text.literal(" ➤ ").formatted(Formatting.BOLD).formatted(Formatting.GRAY))
+                        .append(Text.literal("Ping: ").formatted(Formatting.BOLD).formatted(Formatting.YELLOW))
+                        .append(Text.literal(String.valueOf(ping)).formatted(Formatting.BOLD).formatted(Formatting.GREEN))
+                        .append(Text.literal(" ms").formatted(Formatting.BOLD).formatted(Formatting.YELLOW))
+                    , false);
+            }
+        }
+        return 1;
+    }
+
+    public static int getPing() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        ClientPlayNetworkHandler networkHandler = client.getNetworkHandler();
+        if (networkHandler != null) {
+            PlayerListEntry playerListEntry = networkHandler.getPlayerListEntry(client.player.getUuid());
+            if (playerListEntry != null) {
+                return playerListEntry.getLatency();
+            }
+        }
+        return -1; // Return -1 if ping cannot be determined
+    }
+}
